ccff4432c0bec1a5f7807bb5bd23053ffaa08974
narek <narekhunanyan0497@gmail.com>
2026-04-08 16:22:47 +0400

    change

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', process.env.FRONTEND_URL || ''].filter(
-      Boolean,
-    ),
+    origin: [
+      'http://localhost:5173',
+      'https://hayfin-front.vercel.app',
+      process.env.FRONTEND_URL || '',
+    ].filter(Boolean),
     credentials: true,
   });
 
