1ee28d51d38eaafd37dee816a08d51f8cf9158c8
narek <narekhunanyan0497@gmail.com>
2026-04-14 15:24:43 +0400

    fix

--- src/applications/applications.module.ts
+++ src/applications/applications.module.ts
@@ -6,8 +6,9 @@ import { ScoringService } from '../loan-application/scoring.service';
 import { SmsModule } from '../sms/sms.module';
 
 @Module({
+  imports: [SmsModule],
   controllers: [ApplicationsController],
-  providers: [ApplicationsService, PrismaService, ScoringService, SmsModule],
+  providers: [ApplicationsService, PrismaService, ScoringService],
   exports: [ApplicationsService],
 })
 export class ApplicationsModule {}
